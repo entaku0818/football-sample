ea2e2fc5c2200fafaee1826b5787ceb6698f8203
entaku <entaku19890818@gmail.com>
2023-07-30 19:09:24 +0900

    fix

--- football/ContentView.swift
+++ football/ContentView.swift
@@ -46,106 +46,20 @@ class TeamsInteractor: TeamsInteractorInput {
     func fetchTeams(completion: @escaping ([Team]) -> Void) {
         // ダミーのJSONデータをここにペースト
         // サンプルのJSONデータをペースト
-        let jsonString = """
-        {
-            "count": 20,
-            "filters": {},
-            "competition": {
-                "id": 2021,
-                "area": {
-                    "id": 2072,
-                    "name": "England"
-                },
-                "name": "Premier League",
-                "code": "PL",
-                "plan": "TIER_ONE",
-                "lastUpdated": "2022-03-20T08:58:54Z"
-            },
-            "season": {
-                "id": 1564,
-                "startDate": "2023-08-11",
-                "endDate": "2024-05-19",
-                "currentMatchday": 1,
-                "winner": null
-            },
-            "teams": [
-                {
-                    "id": 57,
-                    "area": {
-                        "id": 2072,
-                        "name": "England"
-                    },
-                    "name": "Arsenal FC",
-                    "shortName": "Arsenal",
-                    "tla": "ARS",
-                    "crestUrl": "https://crests.football-data.org/57.png",
-                    "address": "75 Drayton Park London N5 1BU",
-                    "phone": "+44 (020) 76195003",
-                    "website": "http://www.arsenal.com",
-                    "email": "info@arsenal.co.uk",
-                    "founded": 1886,
-                    "clubColors": "Red / White",
-                    "venue": "Emirates Stadium",
-                    "lastUpdated": "2022-02-10T19:48:56Z"
-                },
-                {
-                    "id": 58,
-                    "area": {
-                        "id": 2072,
-                        "name": "England"
-                    },
-                    "name": "Aston Villa FC",
-                    "shortName": "Aston Villa",
-                    "tla": "AVL",
-                    "crestUrl": "https://crests.football-data.org/58.png",
-                    "address": "Villa Park Birmingham B6 6HE",
-                    "phone": "+44 (0121) 3272299",
-                    "website": "http://www.avfc.co.uk",
-                    "email": null,
-                    "founded": 1872,
-                    "clubColors": "Claret / Sky Blue",
-                    "venue": "Villa Park",
-                    "lastUpdated": "2022-04-03T16:22:14Z"
-                }
-            ]
-        }
-        """
 
-        // JSONデータをSwiftのData型に変換
-        if let jsonData = jsonString.data(using: .utf8) {
-            let decoder = JSONDecoder()
-            do {
-                // "teams"を指定してデコード
-                let decodedData = try decoder.decode(TeamsData.self, from: jsonData)
-                let teams = decodedData.teams
+
+        let teamsAPI = TeamsAPI()
+
+        // Call fetchTeams method
+        teamsAPI.fetchTeams { teams in
+            if let teams = teams {
+                // Successfully fetched teams data
                 completion(teams)
-            } catch {
-                print("Error decoding JSON: \(error)")
+            } else {
+                // Failed to fetch teams data
                 completion([])
             }
-        } else {
-            print("Failed to convert JSON string to Data.")
-            completion([])
         }
     }
 }
 
-struct Team: Decodable {
-    let id: Int
-    let name: String
-    let shortName: String
-    let tla: String
-    let crestUrl: String
-    let address: String?
-    let phone: String?
-    let website: String?
-    let email: String?
-    let founded: Int?
-    let clubColors: String?
-    let venue: String?
-    let lastUpdated: String?
-}
-// "teams"に対応するデータ型
-struct TeamsData: Decodable {
-    let teams: [Team]
-}
---
+++ football/network/api.swift
@@ -0,0 +1,92 @@
+//
+//  api.swift
+//  football
+//
+//  Created by 遠藤拓弥 on 30.7.2023.
+//
+
+import Foundation
+import APIKit
+
+// APIリクエスト用のクラス
+class TeamsAPI {
+    // リクエストのエンドポイントURL
+    private let baseURL: URL = URL(string: "https://api.football-data.org/v4/")!
+
+    // APIキー（自分のAPIキーを使用してください）
+    private let apiKey = ""
+
+    // チーム情報取得APIリクエスト
+    func fetchTeams(completion: @escaping ([Team]?) -> Void) {
+        let request = TeamsRequest(baseURL: baseURL, apiKey: apiKey)
+
+        Session.send(request) { result in
+            switch result {
+            case .success(let response):
+                completion(response.teams)
+            case .failure(let error):
+                print("Error fetching teams: \(error)")
+                completion(nil)
+            }
+        }
+    }
+}
+
+// APIリクエストの定義
+struct TeamsRequest: Request {
+
+    var baseURL: URL
+
+    typealias Response = TeamsData
+
+    let apiKey: String
+
+    var path: String {
+        return "competitions/PL/teams/"
+    }
+
+    var method: HTTPMethod {
+        return .get
+    }
+
+    var headerFields: [String: String] {
+        return ["X-Auth-Token": apiKey]
+    }
+
+    func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        guard let data = try? JSONSerialization.data(withJSONObject: object),
+              let teamsData = try? JSONDecoder().decode(TeamsData.self, from: data) else {
+            throw ResponseError.unexpectedObject(object)
+        }
+        return teamsData
+    }
+
+}
+struct Team: Decodable {
+    let id: Int
+    let name: String
+    let shortName: String
+    let tla: String
+    let crest: String
+    let address: String?
+    let phone: String?
+    let website: String?
+    let email: String?
+    let founded: Int?
+    let clubColors: String?
+    let venue: String?
+    let lastUpdated: String?
+}
+// "teams"に対応するデータ型
+struct TeamsData: Decodable {
+    let teams: [Team]
+
+    private enum CodingKeys: String, CodingKey {
+        case teams = "teams"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        teams = try container.decode([Team].self, forKey: .teams)
+    }
+}
